e9d386d9d5ead8938a8a74e7667376ceb2cc69d9
Nicolas Chenet <nicolas@balloonup.com>
2012-03-31 00:56:07 +0200

    Cleaner global vars management

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ var express = require('express')
   , app     = express.createServer()
   , io      = require('socket.io').listen(app);
 
+var app_settings = {
+    author :      'Nicolas Chenet'
+  , github_repo : 'https://github.com/nicolaschenet/ConnectedDashboard'
+}
+
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
-    app.set('view options', { layout : false })
+    app.set('view options', {
+      layout : false,
+      settings : app_settings
+    });
 });
 
 app.use('/static', express.static(__dirname + '/static'));
@@ -13,11 +21,7 @@ app.use('/static', express.static(__dirname + '/static'));
 app.listen(1337);
 
 app.get('/', function (req, res) {
-  res.render('index', {
-      page_title :  'Welcome to ConnectedDashboard'
-    , author :      'Nicolas Chenet'
-    , github_repo : 'https://github.com/nicolaschenet/ConnectedDashboard'
-  });
+  res.render('index', { page_title :  'Welcome to ConnectedDashboard' });
 });
 
 io.sockets.on('connection', function (socket) {
