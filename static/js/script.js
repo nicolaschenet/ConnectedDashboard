0b50c37828b9c89486f87c365ec57e890e1f85c2
Nicolas Chenet <nicolas@balloonup.com>
2012-03-31 03:20:44 +0200

    Better scripts placement

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,13 @@
 /* Author:
-
+    Nicolas Chenet - nicolas@balloonup.com
 */
 
-
+jQuery(document).ready(function() {
+    var socket = io.connect();
+    socket.on('connected', function(data){
+        console.log(data.message);
+    });
+});
 
 
 
