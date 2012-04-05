0a56e21364aa4ccf28f814d986cde583584bee42
Nicolas Chenet <nicolas@balloonup.com>
2012-04-05 19:53:18 +0200

    Changed secret

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ secret: 'thisisnotsosecret' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -40,7 +40,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.home);
+app.get('/',  routes.home);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
