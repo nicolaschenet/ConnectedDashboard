b50b5db8e158d38bc8d166d4ca247144cd7a2658
Nicolas Chenet <nicolas@balloonup.com>
2012-05-16 10:52:12 +0200

    fixes #1: multi-client now supported, yeahhh !

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ var express     = require('express')
   , TwitterNode = require('twitter-node').TwitterNode
   , sys         = require('util');
 
+
 /**
  * Configuration
  */
 
-// Generic config
+// App configuration
+
 var app_settings = {
     author :      'Nicolas Chenet'
   , github_repo : 'https://github.com/nicolaschenet/ConnectedDashboard'
@@ -44,59 +46,83 @@ app.configure('production', function(){
 });
 
 
-// Routes
+// Twitter configuration
+
+var twit = new TwitterNode({
+    user:     'nicolaschenet'
+  , password: 'ha6vr851'
+  , track:  [
+        'ConnectedDashboard'
+  ]
+  , follow: [
+        83561264  // @nicolaschenet
+      , 142254877 // @guillaumepotier
+      , 65804544  // @arcanis
+      , 209020852 // @CeD_EF
+      , 98431545  // @balloon
+      , 42241156  // @Romaind
+      , 295052629 // @ToGeoffreyh
+      , 8314462   // @skruk
+  ]
+});
+
+
+/**
+ * Events
+ */
+
+// Twitter events
+
+twit.addListener('error', function(error) {
+  console.log(error.message);
+});
+
+twit
+  .addListener('tweet', function(tweet) {
+    sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
+    io.sockets.emit('tweet', {
+        message : JSON.stringify(tweet)
+    });
+  })
+  .addListener('limit', function(limit) {
+    sys.puts("LIMIT: " + sys.inspect(limit));
+  })
+  .addListener('delete', function(del) {
+    sys.puts("DELETE: " + sys.inspect(del));
+    io.sockets.emit('tweet_deleted', {
+      message: del
+    });
+  })
+  .addListener('end', function(resp) {
+    sys.puts("wave goodbye... " + resp.statusCode);
+  })
+  .stream();
+
+
+// Generic socket events
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('connected', {
+    message: 'Socket is now connected. Enjoy, guys ! ;)'
+  });
+});
+
+
+/**
+ * Routes
+ */
 
 app.get('/',  routes.home);
+
+
+/**
+ * Let's listen to it now ;)
+ **/
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
 
-io.sockets.on('connection', function (socket) {
 
-  socket.emit('connected', {
-    message: 'Socket is now connected. Enjoy, guys ! ;)'
-  });
 
-  var twit = new TwitterNode({
-      user:     'twitter_screen_name'
-    , password: 'twitter_password'
-    , track:    ['ConnectedDashboard, balloon']
-    , follow: [
-          83561264  // @nicolaschenet
-        , 142254877 // @guillaumepotier
-        , 65804544  // @arcanis
-        , 209020852 // @CeD_EF
-        , 98431545  // @balloon
-        , 42241156  // @Romaind
-        , 295052629 // @ToGeoffreyh
-    ]
-  });
-
-  twit.addListener('error', function(error) {
-    console.log(error.message);
-  });
-
-  twit
-    .addListener('tweet', function(tweet) {
-      sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
-      socket.emit('tweet', {
-          message : JSON.stringify(tweet)
-      });
-    })
-    .addListener('limit', function(limit) {
-      sys.puts("LIMIT: " + sys.inspect(limit));
-    })
-    .addListener('delete', function(del) {
-      sys.puts("DELETE: " + sys.inspect(del));
-      socket.emit('tweet_deleted', {
-        message: del
-      });
-    })
-    .addListener('end', function(resp) {
-      sys.puts("wave goodbye... " + resp.statusCode);
-    })
-    .stream();
-
-});
